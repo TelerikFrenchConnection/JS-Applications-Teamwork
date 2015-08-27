93794eba33466af23fe7e975f56b940b5bb2b25a
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-08-27 17:35:49 +0300

    Adding sammy structure

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-import 'jquery';
-import 'sammy';
-import 'q';
+import $ from 'jquery';
+import sammy from 'sammy';
 
 export function init($element) {
+	var app = sammy('#content',function () {
+		this.get('#/home', function () {
+			
+		});
+
+		this.get('#/categories', function () {
+			
+		});
+		
+		this.get('#/categories/:categoryName', function () {
+			var category = this.params['categoryName'];
+		});
+
+		this.get('#/library', function () {
+			
+		});
+		
+		this.get('#/library/:bookId', function () {
+			var bookId = this.params['bookId'];
+			// Load detailed view of book
+		});
+	});
 	
+	app.run('#/');		
 }
