c07a12379b04fccc92e6e519b8cd72e0c8439598
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-08-28 02:10:22 +0300

    Updating dependencies && fixing stylesheet issue with live-server

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ System.config({
 		sammy: 'lib/bower_components/sammy/lib/min/sammy-latest.min.js',
 		q: 'lib/bower_components/q/q.js',
 		chai: 'lib/bower_components/chai/chai.js',
-		mustache: 'lib/bower_components/mustache.js/mustache.min.js',
+		handlebars: 'lib/bower_components/handlebars/handlebars.min.js',
 		underscore: 'lib/bower_components/underscore/underscore-min.js'
 	}
 });
