56e7ddc004df5501d4f61f4ff248974b8c23cfbe
unknown <bask3tt@gmail.com>
2015-08-29 14:56:45 +0300

    Added validations for book and added new function in validatorHelper

--- models/book.js
+++ models/book.js
@@ -1,15 +1,80 @@
 var Book = (function() {
+    import validate from './helpers/validatorHelper.js';
+
     var Book = {
-        init: function(title, author, category, isbn, state, pictureURL, description) {
+        init: function(title, author, category, isbn, price, pictureURL, description) {
             this.title = title;
             this.author = author;
             this.category = category;
             this.isbn = isbn;
-            this.state = state;
+            this.price = price;
             this.pictureURL = pictureURL;
             this.description = description;
             return this;
+        },
+
+        get title() {
+            return this.title;
+        },
+
+        set title(newTitle) {
+            validate.isString(newTitle, 'Title');
+            this._title = newTitle;
+        },
+
+        get author() {
+            return this.author;
+        },
+
+        set author(newAuthor) {
+            validate.isString(newAuthor, 'Author');
+            this._author = newAuthor;
+        },
+
+        get category() {
+            return this.category;
+        },
+
+        set category(newCategory) {
+            validate.isString(newCategory, 'Category');
+            this._category = newCategory;
+        },
+
+        get isbn() {
+            return this.isbn;
+        },
+
+        set isbn(newIsbn) {
+            validate.positiveNumber(newIsbn, 'Isbn');
+            this._isbn = newIsbn;
+        },
+
+        get price() {
+            return this.price;
+        },
+
+        set price(newPrice) {
+            validate.price(newPrice, 'Price');
+            this._price = newPrice;
+        },
+
+        get pictureURL() {
+            return this.pictureURL;
+        },
+
+        set pictureURL(newPictureUrl) {
+            this._pictureUrl = newPictureUrl;
+        },
+
+        get description() {
+            return this.description;
+        },
+
+        set description(newDescription) {
+            validate.isString(newDescription, 'Description');
+            this._description = newDescription;
         }
+
     };
     return Book;
 }());
--- models/helpers/validatorHelper.js
+++ models/helpers/validatorHelper.js
@@ -5,27 +5,27 @@
     USER_NAME_PATTERN: /(^\w+$).{6,15}/,
     EMAIL_PATTERN: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
     MAX_NUMBER: 9007199254740992,
-    DEFALT: 'Value'
+    DEFAULT: 'Value'
 };
 
 validate = {
     ifUndefined: function (value, name) {
-        name = name || CONSTANTS.DEFALT;
+        name = name || CONSTANTS.DEFAULT;
         if (value === undefined) {
             throw new Error(name + ' cannot be undefined');
         }
     },
 
     ifNumber: function (value, name) {
-        name = name || CONSTANTS.DEFALT;
+        name = name || CONSTANTS.DEFAULT;
         if (typeof value !== 'number') {
             throw new Error(name + ' must be a number');
         }
     },
 
     isString: function (value, name) {
-        name = name || CONSTANTS.DEFALT;
-        this.IfUndefined(value, name);
+        name = name || CONSTANTS.DEFAULT;
+        this.ifUndefined(value, name);
 
         if (typeof value !== 'string') {
             throw new Error(name + ' must be a string');
@@ -39,9 +39,9 @@ validate = {
     },
 
     positiveNumber : function (value, name) {
-        name = name || CONSTANTS.DEFALT;
-        this.IfUndefined(value, name);
-        this.IfNumber(value, name);
+        name = name || CONSTANTS.DEFAULT;
+        this.ifUndefined(value, name);
+        this.ifNumber(value, name);
 
         if (value <= 0) {
             throw new Error(name + ' must be positive number');
@@ -50,23 +50,29 @@ validate = {
 
 
     email: function (value, name) {
-        name = name || CONSTANTS.DEFALT;
+        name = name || CONSTANTS.DEFAULT;
         if (!CONSTANTS.EMAIL_PATTERN.test(value))
             throw new Error(name + ' is Invalid')
     },
 
     password: function (value, name) {
-        name = name || CONSTANTS.DEFALT;
+        name = name || CONSTANTS.DEFAULT;
         if (!CONSTANTS.PASSWORD_PATTERN.test(value)) {
             throw new Error(name + ' must be between 8-20 symbols and to contains at least one: Uppercase letter, lowcase letter, and number ')
         }
     },
 
     userName: function (value, name) {
-        name = name || CONSTANTS.DEFALT;
+        name = name || CONSTANTS.DEFAULT;
         if (!CONSTANTS.USER_NAME_PATTERN(name)) {
             throw new Error(name + ' must be between 6-15 symbols and can contain only letters, numbers and underscore')
         }
+    },
+
+    price: function (value, name) {
+        name = name || CONSTANTS.DEFAULT;
+        this.ifUndefined(value, name);
+        this.ifNumber(value, name);
     }
 };
 
