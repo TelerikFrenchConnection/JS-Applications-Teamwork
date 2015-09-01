33c07c21e18272bc790a5583e07c66f112bb5df8
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-09-01 23:57:14 +0300

    Adding npm/bower version

--- config.js
+++ config.js
@@ -1,65 +1,19 @@
 System.config({
-  baseURL: "/",
-  defaultJSExtensions: true,
-  transpiler: "babel",
-  babelOptions: {
-    "optional": [
-      "runtime"
-    ]
-  },
-  paths: {
-    "github:*": "lib/github/*",
-    "npm:*": "lib/npm/*",
-    "dbContext": "./models/database/dbContext.js"
-  },
-
-  map: {
-    "babel": "npm:babel-core@5.8.22",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
-    "bootstrap": "github:twbs/bootstrap@3.3.5",
-    "core-js": "npm:core-js@0.9.18",
-    "handlebars": "github:components/handlebars.js@3.0.3",
-    "jquery": "github:components/jquery@2.1.4",
-    "parse": "github:albemuth/parse-js-sdk@1.4.2",
-    "sammy": "npm:knottie-sammy@0.7.6",
-    "underscore": "npm:underscore@1.8.3",
-    "viewer": "npm:viewer@0.10.10",
-    "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.4.3"
-    },
-    "github:jspm/nodelibs-path@0.1.0": {
-      "path-browserify": "npm:path-browserify@0.0.0"
-    },
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
-    },
-    "github:twbs/bootstrap@3.3.5": {
-      "jquery": "github:components/jquery@2.1.4"
-    },
-    "npm:babel-runtime@5.8.20": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:buffer@3.4.3": {
-      "base64-js": "npm:base64-js@0.0.8",
-      "ieee754": "npm:ieee754@1.1.6",
-      "is-array": "npm:is-array@1.0.1"
-    },
-    "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:knottie-sammy@0.7.6": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:path-browserify@0.0.0": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:viewer@0.10.10": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    }
-  }
+	transpiler: "babel",
+	babelOptions: {
+		optional: [
+			"runtime"
+		]
+	},
+	map: {
+		"dbContext": "./models/database/index.js",
+		babel: 'lib/node_modules/babel-core/browser.min.js',
+		jquery: 'lib/bower_components/jquery/dist/jquery.min.js',
+		sammy: 'lib/bower_components/sammy/lib/sammy.js',
+		q: 'lib/bower_components/q/q.js',
+		chai: 'lib/bower_components/chai/chai.js',
+		handlebars: 'lib/bower_components/handlebars/handlebars.min.js',
+		underscore: 'lib/bower_components/underscore/underscore-min.js',
+		parse: 'lib/bower_components/parse-1.5.0/index.js'
+	}
 });
