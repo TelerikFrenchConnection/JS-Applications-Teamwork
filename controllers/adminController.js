f3cf05aae13c3bd8cd22693fb68d89c2a87393e2
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-09-02 15:54:59 +0300

    Fixing not-logged-in verification console error

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -20,7 +20,9 @@ class adminController {
 }
 
 function isUserAuthorized(sammy) {
-    if(!Parse.User.current().attributes.isAdmin) {
+    if(!Parse.User.current()) {
+        sammy.redirect('#/404');
+    } else if (!Parse.User.current().attributes.isAdmin) {
         sammy.redirect('#/404');
     }
 }
