f465ca92baf73b87efc34af586d0e532ff7f7bbf
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-09-03 02:19:26 +0300

    Adding latest books to home.html

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,9 +4,22 @@ import db from 'dbContext';
 import templatesHelper from '../views/helpers/templatesHelper.js';
 import pagesHelper from '../views/helpers/pagesHelper.js';
 
+import bookModel from '../models/bookModel.js';
+
 class homeController {
-    load() {
+    load(sammy) {
         pagesHelper.append('home');
+        bookModel.getBooks().limit(3).find()
+            .then(function(books){
+                return templatesHelper.append('libraryBook', books, '#library-content');
+            })
+            .then(function() {
+                var libraryBookContent = $('#library-content');
+                libraryBookContent.on('click', 'div img', function() {
+                    var id = $(this).attr('data-id');
+                    sammy.redirect('#/library/detailed/' + id);
+                });
+            });
     }
 }
 
